8f589117fa24fda3da3ea0b74bd606b26d6be201
1445654576@qq.com <1445654576@qq.com>
2020-10-30 00:14:46 +0800

    完成宽高自适应

--- src/components/al-components/al-image/ALImage.js
+++ src/components/al-components/al-image/ALImage.js
@@ -1,33 +1,84 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, View} from 'react-native';
 import PropTypes from 'prop-types';
+
 
 class ALImage extends React.Component {
 
   //构造器
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      src: null,
+      vw: 0,
+      vh: 0,
+      width: 0,
+      height: 0
+    };
+  }
+
+  //根View的onLayout回调函数
+  onLayout = (event) => {
+    //获取根View的宽高，以及左上角的坐标值
+    let {width, height} = event.nativeEvent.layout;
+    this.setState({vw: Math.floor(width), vh: Math.floor(height)});
+    this.setWH();
+  }
+
+  setWH = () => {
+
+    // 本地图片
+    if (typeof this.props.src === 'number') {
+      let img = Image.resolveAssetSource(this.props.src);
+      this.setState({
+        width: img.width,
+        height: img.height
+      });
+    }
+
+    // 网络图片
+    if (typeof this.props.src !== 'number') {
+      Image.getSize(this.props.src,
+        (width, height) => {
+          this.setState({
+            width: this.state.vw,
+            height: Math.floor(this.state.vw/width*height),
+          });
+        },
+        err => {
+          console.log('net image err', err);
+        });
+    }
   }
 
   // 渲染函数
   render() {
+    let {src, width, height, vw, vh} = this.state;
     const props = this.props;
+    let iw = vw;
+    let ih = Math.floor(vw / width * height);
 
     return (
-      <Image source={props.src !== '' ? props.src : {uri: props.url}}
-             style={{
-               width: props.width,
-               height: props.height,
-               borderRadius: props.round ? props.width : props.radius,
-               ...props.style,
-             }}/>
+      <View  onLayout={this.onLayout}>
+        <Image source={src}
+               resizeMode={props.fit}
+               style={{
+                 width: props.width ?? (iw ? iw : width),
+                 height: props.height ?? (ih ? ih : height),
+                 borderRadius: props.round ? props.width : props.radius,
+                 ...props.style,
+               }}/>
+      </View>
     );
   }
+
 
   // 生命周期函数
   //组件已挂载
   componentDidMount() {
+    this.setState({
+      src: (typeof this.props.src === 'number') ? this.props.src : {uri: this.props.src},
+    });
 
   }
 
@@ -35,7 +86,6 @@ class ALImage extends React.Component {
   componentWillUnmount() {
 
   }
-
 
 }
 
@@ -63,9 +113,6 @@ ALImage.propTypes = {
 
 // prop默认值
 ALImage.defaultProps = {
-  src: '',
-  width: 200,
-  height: 200,
   fit: 'cover',
   round: false,
   style: {},
